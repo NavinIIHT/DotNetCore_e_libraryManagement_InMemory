2d4ebe875f224793fa021a12a919427bb752b6df
Uma Kumar <define_navin@rediffmail.com>
2021-11-22 16:31:09 +0530

    InitialCommit

---
+++ e-library.Test/TestCases/ExceptionalTest.cs
@@ -0,0 +1,174 @@
+﻿using e_library.BusinessLayer.Interfaces;
+using e_library.BusinessLayer.Services;
+using e_library.BusinessLayer.Services.Repository;
+using e_library.Entities;
+using e_library.Test.Utlities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace e_library.Test.TestCases
+{
+    public class ExceptionalTest
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly ILibraryServices _libraryS;
+        public readonly Mock<ILibraryRepository> libraryservice = new Mock<ILibraryRepository>();
+        private Book _book;
+        private Student _student;
+        private Book_Issue _IssueBook;
+        public ExceptionalTest(ITestOutputHelper output)
+        {
+            _libraryS = new LibraryServices(libraryservice.Object);
+            _output = output;
+            _book = new Book()
+            {
+                Id = 1,
+                BookName = "Physics - 1",
+                ISBN = "10091",
+                Author = "K C Sinha",
+                Publisher = "Sinha",
+                Published_Year = 1990,
+                Edition = "First",
+                Streams = Streams.Science,
+                Issued = true
+            };
+            _student = new Student()
+            {
+                Id = 1,
+                Name = "Kundan",
+                Email = "umakumarsingh@techademy.com",
+                streams = Streams.Science,
+                Phone = 9631438123,
+                DOB = new DateTime(1990, 03, 01),
+                Address = "Banglore"
+            };
+            _IssueBook = new Book_Issue()
+            {
+                Id = 1,
+                BookId = 1,
+                StudentId = 1,
+                Issue_Date = DateTime.Now,
+                Return_Date = DateTime.Now.AddDays(7),
+                ActualReturn_Date = DateTime.Now,
+                Fine = 0,
+                Returned = false
+            };
+        }
+        /// <summary>
+        /// Creating test output text file that store the result in boolean result
+        /// </summary>
+        static ExceptionalTest()
+        {
+            if (!File.Exists("../../../../output_exception_revised.txt"))
+                try
+                {
+                    File.Create("../../../../output_exception_revised.txt").Dispose();
+                }
+                catch (Exception)
+                {
+
+                }
+            else
+            {
+                File.Delete("../../../../output_exception_revised.txt");
+                File.Create("../../../../output_exception_revised.txt").Dispose();
+            }
+        }
+        /// <summary>
+        /// Test to validate invalid book add if book object is null.
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task<bool> Testfor_Validate_InvlidUserBookAdd()
+        {
+            //Arrange
+            bool res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            _book = null;
+            //Act
+            try
+            {
+                libraryservice.Setup(repo => repo.AddBook(_book)).ReturnsAsync(_book = null);
+                var result = await _libraryS.AddBook(_book);
+                if (result == null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Asert
+                //final result displaying in text file if exception occure
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await File.AppendAllTextAsync("../../../../output_exception_revised.txt", "Testfor_Validate_InvlidUserBookAdd=" + res + "\n");
+                return false;
+            }
+            //Asert
+            //final result save in text file.
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await File.AppendAllTextAsync("../../../../output_exception_revised.txt", "Testfor_Validate_InvlidUserBookAdd=" + res + "\n");
+            return res;
+        }
+        /// <summary>
+        /// Test to validate invalid student registration if student object is null.
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task<bool> Testfor_Validate_InvlidUserStudentRegister()
+        {
+            //Arrange
+            bool res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            _student = null;
+            //Act
+            try
+            {
+                libraryservice.Setup(repo => repo.Register(_student)).ReturnsAsync(_student = null);
+                var result = await _libraryS.Register(_student);
+                if (result == null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Asert
+                //final result displaying in text file if exception occure
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await File.AppendAllTextAsync("../../../../output_exception_revised.txt", "Testfor_Validate_InvlidUserStudentRegister=" + res + "\n");
+                return false;
+            }
+            //Asert
+            //final result save in text file.
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await File.AppendAllTextAsync("../../../../output_exception_revised.txt", "Testfor_Validate_InvlidUserStudentRegister=" + res + "\n");
+            return res;
+        }
+    }
+}
---
+++ e-library.Test/Utlities/GetToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace e_library.Test.Utlities
+{
+    public class GetToken
+    {
+        public string token { get; set; }
+        public string error { get; set; }
+    }
+}
